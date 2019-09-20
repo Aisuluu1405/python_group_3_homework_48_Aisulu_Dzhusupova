d81b4ccbd6a0fe9a93c9bdd6723f29268b5bbdb9
Айсулу <aisuluueco2009@yandex.ru>
2019-09-20 16:13:30 +0600

    Create index page(view, url, html,style.css) and change base.html

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+# from webapp.forms import ProductForm
+from webapp.models import Product
 
-# Create your views here.
+
+def index_view(request, *args, **kwargs):
+    products = Product.objects.all()
+    return render(request, 'index.html', context={
+        'products': products
+    })
+
