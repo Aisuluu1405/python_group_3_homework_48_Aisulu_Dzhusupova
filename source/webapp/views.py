39f04733848b6ad85c305eec695cf28efc894a6d
Айсулу <aisuluueco2009@yandex.ru>
2019-09-20 23:06:47 +0600

    Create page product_view, and add change index.html

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -4,8 +4,17 @@ from webapp.models import Product
 
 
 def index_view(request, *args, **kwargs):
-    products = Product.objects.all()
+    products = Product.objects.filter(count__gt=0).order_by('category', 'name')
     return render(request, 'index.html', context={
         'products': products
     })
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_view.html', context={
+        'product': product
+    })
+
+
 
