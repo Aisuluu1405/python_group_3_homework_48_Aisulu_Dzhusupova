042e1bdb258680516709f2f97f7328c8ca6f1d15
Айсулу <aisuluueco2009@yandex.ru>
2019-09-20 11:40:18 +0600

    Change admin.py, add class ProductAdmin

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from webapp.models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'count', 'price']
+    list_filter = ['category']
+    list_display_links = ['name']
+    search_fields = ['category']
+    exclude = []
+
+
+admin.site.register(Product, ProductAdmin)
+
